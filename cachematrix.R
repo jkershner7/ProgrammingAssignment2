8f654994e47b29269b6f0980c40b6a0f552f279b
jkershner7 <64991302+jkershner7@users.noreply.github.com>
2020-05-07 17:22:16 -0600

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# # This function is used to create a specail "matrix"
+## object that can cache its inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  create <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  calc <- function() x
+  createInverse <- function() inv <<-solve(x)
+  calcInverse <- function() inv
+  list(create = create, calc = calc, createInverse = createInverse, calcInverse = calcInverse)
 }
 
 
-## Write a short comment describing this function
+## This function uses the inverse calculated from makeCacheMatrix. If the inverse was 
+## was already found and the matrix hasn't changed then cachesolve will
+## calculate the inverse using the cache. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$calcInverse()
+    if (!is.null(inv)) {
+      message("cached data")
+      return(inv)
+    }
+    matrix <- x$calc()
+    inv <- solve(matrix, ...)
+    x$createInverse(inv)
+    inv
 }
